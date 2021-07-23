8d178ab6083c5f9541e2ab93b366d8546ab88859
Will Puricz <wilhelm.puricz@gmail.com>
2021-07-23 16:03:40 -0400

    Formatted 2 columns for detail page

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -6,8 +6,8 @@ import { useEffect, useState } from 'react';
 import queryString from 'query-string'
 import { useLocation } from 'react-router-dom'
 
-const Detail = () => {
-	// page content
+const Detail = (props) => {
+	// page contents
 	const pageTitle = 'Detail'
 	const pageDescription = 'Welcome to Go! NFT Yourself'
 	const params = new URLSearchParams(window.location.search);
@@ -22,25 +22,20 @@ const Detail = () => {
             .then(res => {
                 setAssetDetails(res.data)
             })
-}, [])
+	}, [])
 
-			
-	
-
-   
-		return (
-			<div>
-
-				<h2>{assetDetails.name}</h2>
-				
-
- 
-{assetDetails.description}
-
-<img src={assetDetails.image_url}></img>
-
+	return (
+		<div class="row">
+			<div class="col-xl-6 col-lg-6 col-md-8 col-sm-12 mx-auto">
+				<img src={assetDetails.image_url}></img>
+				{/* <img src="https://lh3.googleusercontent.com/Z_ApIlZUdb4VclOU4-Z8O7EkYCZZir4OXJqGYnuH3Wn9oyFlit-1Fsu_IqomX_stEfUHzs1lfr6rgUWxgTZ63F1kIZTVmzaF7t4-yg=w600"/> */}
 			</div>
-		)
-	}
+			<div class="col-xl-6 col-lg-6 col-md-8 col-sm-12 mx-auto">
+				<h2>{assetDetails.name}</h2>
+				<div>{assetDetails.description}</div>
+			</div>
+		</div>
+	)
+}
 
 export default Detail;
