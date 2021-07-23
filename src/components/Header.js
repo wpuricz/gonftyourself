578ff77cf1f94bc565e9725027bfbd7850122f4f
Ahmed, Mohammed <Mohammed.Ahmed@ehi.com>
2021-07-23 18:00:05 -0400

    Collection page refined with card layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,27 +3,11 @@ import { Container } from 'react-bootstrap'
 const Header = ({ head, description }) => {
   return (
     <Container>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    
-<meta charset="utf-8"/>
-<title>Car Deco: Limited Edition NFT's — carbureted - classic car art</title>
-<link rel="shortcut icon" type="image/x-icon" href="https://assets.squarespace.com/universal/default-favicon.ico"/>
-<link rel="canonical" href="https://carbureted.com/nft-contact"/>
-<meta property="og:site_name" content="carbureted - classic car art"/>
-<meta property="og:title" content="Car Deco: Limited Edition NFT's — carbureted - classic car art"/>
-<meta property="og:url" content="https://carbureted.com/nft-contact"/>
-<meta property="og:type" content="website"/>
-<meta itemprop="name" content="Car Deco: Limited Edition NFT's — carbureted - classic car art"/>
-<meta itemprop="url" content="https://carbureted.com/nft-contact"/>
-<meta name="twitter:title" content="Car Deco: Limited Edition NFT's — carbureted - classic car art"/>
-<meta name="twitter:url" content="https://carbureted.com/nft-contact"/>
-<meta name="twitter:card" content="summary"/>
-<meta name="description" content=""/>
-<link rel="preconnect" href="https://images.squarespace-cdn.com"/>
-<link rel="stylesheet" type="text/css" href="//assets.squarespace.com/universal/styles-compressed/commerce-0418f029ba9aeb9db8790-min.en-US.css"/>
-  </Container>
+      <div className='starter-template text-center mt-5'>
+        <h1>{head} Page</h1>
+        <p className='lead text-capitalize'>{description}</p>
+      </div>
+    </Container>
   )
 }
 
