3ed3579cb42c36c012725753a69a5c80f375f176
Will Puricz <wilhelm.puricz@gmail.com>
2021-07-24 21:26:20 -0400

    Formatting in detail screen

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import queryString from 'query-string'
 import { useLocation } from 'react-router-dom'
+import '../index.css'
 
 const Detail = (props) => {
 	// page contents
@@ -14,8 +15,10 @@ const Detail = (props) => {
 	const asset_contract_address = params.get('asset_contract_address')
 	const token_id = params.get('token_id')
 	const [assetDetails, setAssetDetails] = useState([])
+	const imageSuffix = (window.screen.width > 1024) ? '=w600' : '';
 
 	useEffect(() => {
+		
 		var url = 'https://rinkeby-api.opensea.io/api/v1/asset/'+asset_contract_address + '/' + token_id;
 		console.log(`fetching data + ${url}`)
 		axios.get(url)
@@ -26,11 +29,10 @@ const Detail = (props) => {
 
 	return (
 		<div class="row">
-			<div class="col-xl-6 col-lg-6 col-md-8 col-sm-12 mx-auto">
-				<img src={assetDetails.image_url}></img>
-				{/* <img src="https://lh3.googleusercontent.com/Z_ApIlZUdb4VclOU4-Z8O7EkYCZZir4OXJqGYnuH3Wn9oyFlit-1Fsu_IqomX_stEfUHzs1lfr6rgUWxgTZ63F1kIZTVmzaF7t4-yg=w600"/> */}
+			<div class="col-xl-6 col-lg-6 col-md-8 col-sm-12 mx-auto detail-bottom-margin detail-image-margin">
+				<img src={assetDetails.image_url + imageSuffix}></img>
 			</div>
-			<div class="col-xl-6 col-lg-6 col-md-8 col-sm-12 mx-auto">
+			<div class="col-xl-6 col-lg-6 col-md-8 col-sm-12 mx-auto detail-bottom-margin">
 				<h2>{assetDetails.name}</h2>
 				<div>{assetDetails.description}</div>
 			</div>
