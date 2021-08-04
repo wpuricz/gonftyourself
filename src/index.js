c48c90d82666f7aaa9afe39e3b6dafb35c115329
Ahmed, Mohammed <Mohammed.Ahmed@ehi.com>
2021-08-04 10:23:51 -0400

    ADDED css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css'
+import './css/bootstrap.css'
 
-import './index.css';
+import './css/index.css';
+import './css/all.css';
+import './css/mdb-ecommerce.css';
+import './css/mdb-pro.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
