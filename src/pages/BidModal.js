aa50f6baf13f47dacc8d79c68917dd2f001da39e
Will Puricz <wilhelm.puricz@gmail.com>
2021-08-06 08:54:11 -0400

    Added uniswap iframe for modal

--- src/pages/BidModal.js
+++ src/pages/BidModal.js
@@ -1,18 +1,47 @@
 import { useEffect, useState } from 'react';
-import { Button, Modal, Form } from 'react-bootstrap'
-
+import { Button, Modal, Form, CloseButton } from 'react-bootstrap'
+//https://getbootstrap.com/docs/5.0/components/modal/
 const BidModal = (props) => {
   
   const { handleClose, show, placeBid, price, name} = props;
   const [bid,setBid] = useState();
+  const [screenId, setScreenId] = useState(0);
   
   const bidPressed = () => {
+    setScreenId(1); // detect here if we should show the uniswap screen
+    return;
     if(isNaN(bid)) {
         alert('please enter number')
     } else{
         handleClose();
         placeBid(bid);
     }
+  }
+
+  const formComponent = () => {
+    return (
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+            <Form.Label>Bid Price</Form.Label>
+            <Form.Control type="text" placeholder={price} value={bid} onChange={e => setBid(e.target.value)} />
+            <Form.Text className="text-muted">
+            Enter bid for { name }
+            </Form.Text>
+        </Form.Group>
+        </Form>
+    )
+  }
+
+  const uniswapComponent = () => {
+    return (
+      <iframe
+  src="https://app.uniswap.org/#/swap?outputCurrency=0xc778417E063141139Fce010982780140Aa0cD5Ab&inputCurrency=ETH&exactAmount=0.1"
+  height="660px"
+  width="100%"
+  style={{'borderRadius':10 }}
+  id="myId"
+/>
+    )
   }
 
 
@@ -27,15 +56,7 @@ const BidModal = (props) => {
         <Modal.Title>Place Bid</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <Form>
-        <Form.Group controlId="formBasicEmail">
-            <Form.Label>Bid Price</Form.Label>
-            <Form.Control type="text" placeholder={price} value={bid} onChange={e => setBid(e.target.value)} />
-            <Form.Text className="text-muted">
-            Enter bid for { name }
-            </Form.Text>
-        </Form.Group>
-        </Form>
+        {screenId === 0 ? formComponent() : uniswapComponent()}
         </Modal.Body>
         <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
